44ebed530e36146737ef9ea426011dbd07d50905
TurboBelka <belka.atakuet@gmail.com>
2019-12-10 20:14:49 +0200

    updated test1

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@
 def manage_sqrt_task(value):
     result = app.send_task('tasks.square_root', args=(value,))
     print(result.ready())
-    print(result.ready())
+    print(result.ready(111))
     print(result.get(timeout=2))
 
 
